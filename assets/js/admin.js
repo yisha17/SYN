d8717ad457f43fb777058ae3a032e3ca93b89951
saron-solomon <it.saron.solomon@gmail.com>
2021-03-03 17:29:08 +0300

    booking.html edited

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -22,25 +22,53 @@ const carType = document.querySelector('.car-type');
 const plateNumber = document.querySelector('.plate-number');
 const carPrice = document.querySelector('.car-price');
 const addCar = document.querySelector('.btn-adder');
+const urlParams = new URLSearchParams(window.location.search);
+const id = Number(urlParams.get('id'));
 
 
 addCar.onclick =(event) =>{
+    if(id){
+        console.log("clicked");
+        console.log(id);
+
+        db.car_table.update(id,{
+                    
+            car_name:carName.value,
+            plate_number:plateNumber.value , 
+            price:carPrice.value,
+            car_type:carType.value
+        
+        }).then((updated)=>{
+            console.log("updated");
+        }).catch((error)=> {
+            console.log(error);
+        });
+        
+    }
+    else{
+
+        if(validateForm() === true){
+            let flag = bulkcreate(db.car_table,{
+                car_name:carName.value,
+                car_image:window.result,
+                car_type:carType.value,
+                price:carPrice.value,
+                plate_number:plateNumber.value,
+                is_rented:false
+            })
+            console.log(carName.value)
+            console.log("created")
+            console.log(flag);
+            carName.value = chooseFile.value= carType.value = carPrice.value = plateNumber.value = "";
+            getData(db.car_table,(data)=>{
+                console.log(data);
+            });
+        }
+    }
+    
     
    
-    let flag = bulkcreate(db.car_table,{
-        car_name:carName.value,
-        car_image:window.result,
-        car_type:carType.value,
-        price:carPrice.value,
-        plate_number:plateNumber.value,
-        is_rented:false
-    })
-    console.log("created")
-    console.log(flag);
-    carName.value = chooseFile.value= carType.value = carPrice.value = plateNumber.value = "";
-    getData(db.car_table,(data)=>{
-        console.log(data);
-    });
+  
 }
 
 browse.addEventListener('click',choosePhoto);
@@ -48,6 +76,7 @@ browse.addEventListener('click',choosePhoto);
 function choosePhoto(){
     chooseFile.click();
     console.log("successful");
+    console.log(carImage.src);
 }
 chooseFile.addEventListener('change',function(){
     const file = this.files[0];
@@ -60,7 +89,7 @@ chooseFile.addEventListener('change',function(){
         reader.addEventListener('load',function(){
             carImage.setAttribute('src',this.result);
             window.result = this.result
-            console.log(window.result);
+            
         });
 
         //console.log(reader.readAsBinaryString(file));
@@ -70,8 +99,7 @@ chooseFile.addEventListener('change',function(){
 });
 
 document.addEventListener('DOMContentLoaded',()=>{
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = Number(urlParams.get('id'));
+   
     if (id){
         addCar.textContent = "Update Car"
         db.car_table.get(id)
@@ -80,23 +108,28 @@ document.addEventListener('DOMContentLoaded',()=>{
             carPrice.value = data.price;
             carType.value = data.car_type;
             plateNumber.value = data.plate_number;
-            carImage.src = data.car_image;
-            const reader = new FileReader();
-            carImage = reader.readAsDataURL(data.car_image)
+            carImage.setAttribute ('src',data.car_image);
         });
 
-        addCar.onclick =(e) =>{
-            db.car_table.update(id,{car_name:carName.value,plate_number:plateNumber.value,price:carPrice.value,car_type:carType.value})
-            .then(function(updated){
-                console.log("updated");
-            });
-
-        }
+        console.log(window.result);
         
     }
 });
 
-//Database process
+const validateForm = () =>{
+   if(carName.value === ""||
+   carPrice.value ===""||
+   carType.value ===""||
+   carImage.src === ""||
+   carPrice.value === ""||
+   window.result ===undefined){
+       alert("fill the form completely");
+       return false;
+   }
+   else{
+       return true;
+   } 
+}
 
 
 
--- assets/js/manage.js
+++ assets/js/manage.js
@@ -1,75 +1,104 @@
 import CarRentalDB,{
-    bulkcreate,
-    getData,
-    Sortobj
+  bulkcreate,
+  getData,
+  Sortobj
 } from './module.js'
 const carRow = document.querySelector('.car-row');
 const clientRow = document.querySelector('customer-container');
 const tbody = document.getElementById('tbody');
+const carRemove = document.querySelector('.car-delete');
+
 let db = CarRentalDB('CarRentalDB',{
-    car_table:`++id,car_name,car_image,car_type,price,plate_number,is_rented`,
-    user_table:`++id,user_name,email,password,pickup_date,dropup_date,car`
+  car_table:`++id,car_name,car_image,car_type,price,plate_number,is_rented`,
+  user_table:`++id,user_name,email,password,pickup_date,dropup_date,car`
 });
-console.log(db)
+
 
 getData(db.car_table,(data)=>{
-    console.log(data);
+  // console.log(data);
 });
 
 
 
 document.addEventListener('DOMContentLoaded',()=>{
-    let outpost = ''
-    db.car_table.each(car =>{
-        console.log(car.car_image);
-      outpost += `
-      <div class="col-lg-3 mt-5 mb-5">
-      <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="${car.car_image}" alt="Card image cap">
-          <div class="card-body">
-            <h3 class="card-title text-center">${car.car_name}</h3>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">${car.car_type}</li>
-              <li class="list-group-item">${car.price}birr/day</li>
-              <li class="list-group-item">${car.is_rented}</li>
-            </ul>
-            <a href="admin.html?id=${car.id}"><i class="fa fa-edit"></i></a>
-            <a href=""><i class="fa fa-trash"></i></a><br>
-          </div>
+  const customerRemove = document.getElementById('cust-delete');
+  let outpost = ''
+  db.car_table.each(car =>{
+      // console.log(car.car_image);
+    outpost += `
+    <div class="col-lg-3 mt-5 mb-5">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${car.car_image}" alt="Card image cap">
+        <div class="card-body align-center">
+          <h3 class="card-title text-center">${car.car_name}</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${car.car_type}</li>
+            <li class="list-group-item">${car.price}birr/day</li>
+            <li class="list-group-item">${(car.is_rented)? 'Rented':'Available' }</li>
+          </ul>
+          <a href="admin.html?id=${car.id}"><i class="fa fa-edit"></i></a>
+          <a href="#" data-id ="${car.id}" class="car-delete"><i class="fa fa-trash"></i></a><br>
         </div>
-  </div>
-      `; 
-      carRow.innerHTML = outpost;
+      </div>
+</div>
+    `; 
+    carRow.innerHTML = outpost;
 
 
-    })
+  })
+  
+  
+  
+  let tb = ''
+  db.user_table.each(user => {
+    console.log(user)
     
-    
-    
-    let tb = ''
-    db.user_table.each(user => {
-      console.log(user)
-      tb += `
-      <tr>
-      <th scope="row">${user.id}</th>
-      <td>${user.user_name}</td>
-      <td>${user.email}</td>
-      <td>${user.password}</td>
-      <td>${user.pickup_date}</td>
-      <td>${user.dropup_date}</td>
-      <td>${user.car.id}</td>
-      <td>${(user.dropup_date - user.pickup_date) * user.car.price}</td>
-      <td><a class="btn btn-danger"><i class="fa fa-trash"></i></a></td>
-    </tr>
-      `;
+    tb += `
+    <tr>
+    <th id="id" scope="row">${user.id}</th>
+    <td>${user.user_name}</td>
+    <td>${user.email}</td>
+    <td>${user.password}</td>
+    <td>${user.pickup_date}</td>
+    <td>${user.dropup_date}</td>
+    <td>${user.car.car_name}</td>
+    <td>${(dayjs(user.dropup_date).diff(dayjs(user.pickup_date),"day")) * parseInt(user.car.price)} Birr</td>
+    <td><a id="cust-delete" class="btn btn-danger  customer-delete"><i class="fa fa-trash"></i></a></td>
+  </tr>
+    `;
 
-      tbody.innerHTML = tb;
+    tbody.innerHTML = tb;
 
-     
-    })
+   
+  })
+
+  var datev = new Date();
+  
+  var date = dayjs(datev).toISOString();
+  
+  var rdate = date.slice(0,10);
+
+  
+  db.user_table.where('dropup_date').equals(rdate).delete()
+  .then((deleted)=>{
+    console.log("deleted");
+  })
+
+  
+  setTimeout( carRemove.addEventListener('click',()=>{
+    let id = Number(e.target.getAttribute('data-id'));
+    console.log(id);
+
+  }),6000);
+ 
 
 });
 
 
 
-    
+
+
+
+
+
+  
